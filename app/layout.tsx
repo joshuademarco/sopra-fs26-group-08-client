4af38e67c3c51fdb3b8cd43e6edaee9295d929d9
Joshua De Marco <joshua.dm@hispeed.ch>
2026-03-20 23:11:59 +0100

    added group name to main page for end of M2

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Student XX-XXX-XXX",
+  title: "sopra-fs-group-08",
   description: "sopra-fs26-template-client",
 };
 
