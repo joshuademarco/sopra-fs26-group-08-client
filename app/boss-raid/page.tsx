80d7b98a234c75af3bff8036fb59bd3d4c09407c
michael schweizer <michael.p.schweizer@hispeed.ch>
2026-04-17 14:23:35 +0200

    closes #6 Implemeted RaidBoss components, will need some changes after backend is implemented

--- app/boss-raid/page.tsx
+++ app/boss-raid/page.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function BossRaidPage() {
   return (
-    <div className="p-8">
+    <div className="flex flex-col gap-6 p-8">
       <h1 className="text-2xl font-bold">Boss Raid</h1>
-      <p>This is a placeholder page for Boss Raid.</p>
     </div>
   )
 }
+
