a2d3721c11ed48a1e8a0ca9e9464b1fed55dafd4
Joshua De Marco <joshua.dm@hispeed.ch>
2026-05-04 12:21:06 +0200

    added middleware to fix rnadom redirect since useAuth might not be

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 import Link from 'next/link'
 
-import { Button } from '@/components/ui/button'
-import { Hero } from '@/components/hero'
 import { Footer } from '@/components/footer'
+import { Hero } from '@/components/hero'
+import { Button } from '@/components/ui/button'
 import { getServerAuthUser } from '@/lib/server-auth'
 import { redirect } from 'next/navigation'
 
 export default async function Home() {
-
   const user = await getServerAuthUser()
 
   if (user) {
