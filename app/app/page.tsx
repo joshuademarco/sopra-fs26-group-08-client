0617682aada6ad1b29836e3ffafd97c593b15669
Joshua De Marco <joshua.dm@hispeed.ch>
2026-05-04 12:24:55 +0200

    forgot to remove requireServerAuth

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,10 +1,7 @@
 import { getWeather } from '@/actions/weather'
-import { requireServerAuth } from '@/lib/server-auth'
 import ClientApplicationPage from './page.client'
 
 export default async function DashboardPage() {
-  await requireServerAuth('/')
-  
   const weatherCode = await getWeather().catch(() => null)
 
   return <ClientApplicationPage weatherCode={weatherCode} />
