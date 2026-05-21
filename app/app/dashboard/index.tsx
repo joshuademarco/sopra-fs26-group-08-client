b7a8f0df0020f4d3f1467b627cb4899b80a44b92
Joshua De Marco <joshua.dm@hispeed.ch>
2026-05-21 10:56:32 +0200

    reworked live map to use tinySwd asset images for map generation. Added static mpa crafted on tiles creator online mapped and added static animations to for some tiles (sheep, knights, water stones) Improved background to make map blend-in added little cure hover feature to make the sheep happy :)

--- app/app/dashboard/index.tsx
+++ app/app/dashboard/index.tsx
@@ -1,12 +1,15 @@
 import { LiveOnlineMap } from '@/components/live-online-map'
+import { WeatherIcon } from '@/components/weather-icon'
 import { WeatherQuestCard } from '@/components/weather-quest-card'
 
 export default function Dashboard() {
-
   return (
-  <div className='flex flex-1 flex-col gap-4'>
-    <WeatherQuestCard />
-    <LiveOnlineMap />
-  </div>
+    <div className='flex flex-1 flex-col gap-4'>
+      <WeatherQuestCard />
+      {/* <WeatherIcon /> */}
+      <div className='relative -mx-12 -mb-12 flex-1 w-[calc(100%+6rem)]'>
+        <LiveOnlineMap />
+      </div>
+    </div>
   )
 }
