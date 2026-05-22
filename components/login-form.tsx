2ceae1344cde4f3859c2fe7f40756c9d583a2ab5
Joshua De Marco <joshua.dm@hispeed.ch>
2026-05-22 14:50:19 +0200

    removed forgot your password

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -84,12 +84,6 @@ export function LoginForm({
               <Field data-invalid={!!form.formState.errors.password}>
                 <div className="flex items-center">
                   <FieldLabel htmlFor="password">Password</FieldLabel>
-                  <a
-                    href="#"
-                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                  >
-                    Forgot your password?
-                  </a>
                 </div>
                 <Input
                   {...form.register("password")}
